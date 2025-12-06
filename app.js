ed9ee6443cd49d6e591105d61c040c1546a23bdf
Safouan Kerkich <safouan.kerkich@student.ehb.be>
2025-12-06 01:51:17 +0100

    baisc express server working

--- app.js
+++ app.js
@@ -3,31 +3,14 @@ const app = express();
 const cors = require("cors");
 const port = 4000;
 
+
 app.use(express.json());
 
 app.use(cors());
 
+
 app.get("/", (req, res) => {
-    res.send("Hello World GET!");
-});
-
-app.post("/login", (req, res) => {
-    // const {name, password} = req.body {
-    //     res.send(name);
-    // }
-
-    const login = req.body;
-    console.log(login);
-
-    if (login.username !== "Safouan" || login.password !== "Kerkich494") {
-        return res.status(401).json({
-            error: "Invalid credentials (error 401)",
-        });
-    } else {
-        res.send({ message: "Login succesful" });
-    }
-
-    res.send("Hello World POST!");
+    res.send("Working.");
 });
 
 app.listen(port, () => {
