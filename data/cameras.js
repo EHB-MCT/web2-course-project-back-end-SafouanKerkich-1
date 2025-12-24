0b4201492656d71d57e6d2236c5ecec4ba26bd28
Safouan Kerkich <safouan.kerkich@student.ehb.be>
2025-12-24 17:07:57 +0100

    removed fake data with real api data

--- data/cameras.js
+++
@@ -1,6 +0,0 @@
-// Fake camera data
-module.exports = [
-  { id: 1, brand: "Sony", model: "ZV-E10", price: 850 },
-  { id: 2, brand: "Canon", model: "R6 Mark II", price: 2700 },
-  { id: 3, brand: "Fujifilm", model: "X-S20", price: 1299 }
-];
