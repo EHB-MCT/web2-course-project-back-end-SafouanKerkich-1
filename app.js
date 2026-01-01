b7a5e539ed2856ef66c0c57e7e741f12764a39ee
Safouan Kerkich <safouan.kerkich@student.ehb.be>
2026-01-01 23:54:12 +0100

    TASK DONE

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 4000;
+
+const port = process.env.PORT || 4000;
 
 // MongoDB
 const connectDB = require("./db");
